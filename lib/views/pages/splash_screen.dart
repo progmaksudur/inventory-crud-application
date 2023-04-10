dc4ebb0da977c3abb77a67480ea5e4c1f8771ebc
Maksudur Rahman <imriday.456@gmail.com>
2023-04-10 21:12:38 +0600

    Splash Screen Created

--- lib/views/pages/splash_screen.dart
+++ lib/views/pages/splash_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:inventory_crud_application/const/app_color_const.dart';
+import 'package:inventory_crud_application/const/app_helperstring_const.dart';
+import 'package:inventory_crud_application/const/app_textstyles_const.dart';
+import 'package:inventory_crud_application/views/widgets/app_custom_sizebox.dart';
 import 'package:inventory_crud_application/views/widgets/app_customtext_widget.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -11,10 +16,26 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child:AppCustomText(text: "Inventory Management" ,),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AppCustomText(text: AppHelperString.appName,textStyle: AppConstTextStyle.splashScreenText,),
+            AppCustomSizeBox(height: 200.h,),
+            const CircularProgressIndicator(
+              color: AppColor.splashTextColor,
+            )
+
+          ],
+        ),
       ),
     );
   }
