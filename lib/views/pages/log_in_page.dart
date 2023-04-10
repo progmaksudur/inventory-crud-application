3708f098a572d8304fe1b8c2c00e38888ada8e9d
Maksudur Rahman <imriday.456@gmail.com>
2023-04-10 21:14:35 +0600

    2 pages add

---
+++ lib/views/pages/log_in_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LogInScreen extends StatefulWidget {
+  static const String routeName="loginpage";
+  const LogInScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LogInScreen> createState() => _LogInScreenState();
+}
+
+class _LogInScreenState extends State<LogInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
