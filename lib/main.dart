9a68c5afd70af32078745751f3be8cf22dbea44c
Maksudur Rahman <imriday.456@gmail.com>
2023-04-10 16:08:41 +0600

    flutter_screenutil package add and initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:inventory_crud_application/views/pages/splash_screen.dart';
@@ -13,27 +14,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.yellow,
-      ),
-      initialRoute: SplashScreen.routeName,
-      getPages: [
-        GetPage(name: SplashScreen.routeName, page: ()=>SplashScreen()),
-      ],
-
-    );
+    return ScreenUtilInit(
+        designSize:const Size(360, 800),
+        builder: (_, widget) => GetMaterialApp(
+              title: 'Flutter Demo',
+              theme: ThemeData(
+                // This is the theme of your application.
+                //
+                // Try running your application with "flutter run". You'll see the
+                // application has a blue toolbar. Then, without quitting the app, try
+                // changing the primarySwatch below to Colors.green and then invoke
+                // "hot reload" (press "r" in the console where you ran "flutter run",
+                // or simply save your changes to "hot reload" in a Flutter IDE).
+                // Notice that the counter didn't reset back to zero; the application
+                // is not restarted.
+                primarySwatch: Colors.yellow,
+              ),
+              initialRoute: SplashScreen.routeName,
+              getPages: [
+                GetPage(
+                    name: SplashScreen.routeName, page: () => SplashScreen()),
+              ],
+            ));
   }
 }
-
