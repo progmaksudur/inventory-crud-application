1ca446df2b02448952da22088af6b40eade3e895
Maksudur Rahman <imriday.456@gmail.com>
2023-04-10 16:09:33 +0600

    app const textstyle class add

---
+++ lib/const/app_textstyles_const.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:inventory_crud_application/const/app_color_const.dart';
+
+class AppConstTextStyle{
+
+  static TextStyle normalText = TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w500, color: AppColor.primaryTextColor);
+
+
+
+
+
+}
